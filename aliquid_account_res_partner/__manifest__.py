2f835244221233eb0592c54e32c554d64ac77c7c
DigitalAutomations <94461482+DigitalAutomations@users.noreply.github.com>
2023-02-07 14:13:35 +0100

    Add files via upload

---
+++ aliquid_account_res_partner/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Aliquid Account',
+    'description': """
+        Aliquid Account""",
+    'version': '14.0.2',
+    'license': 'LGPL-3',
+    'author': 'DigitalAutomations',
+    'website': 'www.digitalautomations.it',
+    'depends': [
+        'analytic',
+        'account',
+        #'project',
+        'l10n_it_fatturapa_in',
+    ],
+    'data': [
+        'views/account_view.xml',
+        'views/partner_view.xml',
+    ],
+    'sequence': -100,
+}
